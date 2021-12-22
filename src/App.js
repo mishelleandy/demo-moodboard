72bfd0ae1d6272d7ce34fafe494e4d17b1e6237f
LAURIOT <yini.lauriot@gmail.com>
2021-12-22 10:37:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./css/App.scss";
 
 function App() {
+  const giedreImage = useRef();
+  const imariImage = useRef();
+  useEffect(() => {
+    let giedreImagePosition = giedreImage.current.offsetTop;
+    let imariImagePosition = imariImage.current.offsetTop;
+    window.addEventListener("scroll", () => {
+      let scrollPosition = window.pageYOffset;
+      if (scrollPosition > giedreImagePosition) {
+        giedreImage.current.style.transform = `translateY(${
+          (scrollPosition - 200) * 0.1
+        }px)`;
+      }
+      if (scrollPosition > imariImagePosition) {
+        imariImage.current.style.transform = `translateY(${
+          (scrollPosition - imariImagePosition - 400) * 0.1
+        }px)`;
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <section className="hero-wrapper">
+        <img src="./assets/hero.jpg" alt="" />
+        <h1 className="heading">Go to the moon</h1>
+      </section>
+      <section className="title-wrapper">
+        <h2 className="title">Over the moon*</h2>
+        <p className="subtitle">
+          Quatre inconnus se rencontrent dans un ascenseur pour la Lune.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section ref={giedreImage} className="image-wrapper">
+        <img
+          className="image-parallax"
+          srcSet="./assets/giedre-mobile.jpg 640w, ./assets/giedre.jpg 1280w"
+          src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+          alt=""
+        />
+      </section>
+      <section className="overflow" />
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        className="slider-wrapper"
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true, type: "fraction" }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div>
+            <h3 className="title">Première rock pink blossom</h3>
+            <p className="subtitle">
+              La PREMIÈRE ROCK ose le rose avec sa chaîne en acier entrelacée de
+              cuir pastel. En édition limitée.
+            </p>
+            <a href="#" className="link">
+              Découvez
+            </a>
+          </div>
+          <div>
+            <img className="slider-image" src="./assets/slider-1.jpg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <h3 className="title">Première rock pink blossom</h3>
+            <p className="subtitle">
+              La PREMIÈRE ROCK ose le rose avec sa chaîne en acier entrelacée de
+              cuir pastel. En édition limitée.
+            </p>
+            <a href="#" className="link">
+              Découvez
+            </a>
+          </div>
+          <div>
+            <img className="slider-image" src="./assets/slider-2.jpg" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <a href="#" className="button">
+        Découvrir la collection
+      </a>
+      <section ref={imariImage} className="image-wrapper">
+        <img
+          className="image-parallax"
+          srcSet="./assets/imari-mobile.jpg 640w, ./assets/imari.jpg 1280w"
+          src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+          alt=""
+        />
+      </section>
+      <section className="overflow" />
+      <section className="title-wrapper">
+        <h2 className="title">Découvrir les pièces en boutique</h2>
+        <p className="subtitle">
+          Découvrir les nouvelles pièces dans votre boutique CHANEL.
+        </p>
+      </section>
+      <a href="#" className="button">
+        Trouvez votre boutique
+      </a>
     </div>
   );
 }
